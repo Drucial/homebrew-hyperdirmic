7b81516df76c8225ff235052997daf669cf9113f
Drucial <drucial@Mac.attlocal.net>
2025-05-18 16:56:38 -0400

    fix url

--- Casks/hyperdirmic.rb
+++ Casks/hyperdirmic.rb
@@ -2,7 +2,7 @@ cask 'hyperdirmic' do
   version '0.1.1'
   sha256 'a49b95c69e5a17e891dc38c8a48b5042a7ceed26a384fea0b104ebe71aaa377d'
 
-  url 'https://github.com/Drucial/hyperdirmic/releases/download/v0.1.1/Hyperdirmic.zip',
+  url 'https://github.com/Drucial/hyperdirmic-app/releases/download/v0.1.1/Hyperdirmic.zip',
       verified: 'github.com/Drucial/hyperdirmic/'
   name 'Hyperdirmic'
   desc 'Minimal macOS tray app that automatically organizes your Downloads folder'
